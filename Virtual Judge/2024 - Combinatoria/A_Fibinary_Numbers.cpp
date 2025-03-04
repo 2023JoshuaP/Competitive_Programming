16994f07cf4e515bd61ec6baffae8f540ddc7243
2023JoshuaP <jphilco@unsa.edu.pe>
2025-03-04 15:37:02 -0500

    Intento 2 para el problema A, cambiamos la estrategia de solucion, generar primero los valores fibonacci, convertirlos a decimal y proceder a sumarlos para obtener el resultado, paso los 2 primeros test, pero el test 3 genera un wrong answer..

--- Virtual Judge/2024 - Combinatoria/A_Fibinary_Numbers.cpp
+++ Virtual Judge/2024 - Combinatoria/A_Fibinary_Numbers.cpp
@@ -1,37 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string sum_fibinary(string fibinary1, string fibinary2) {
-    string result;
-    int carry = 0;
-    for (int i = 0; i < max(fibinary1.size(), fibinary2.size()); i++) {
-        int sum = carry;
-        if (i < fibinary1.size()) {
-            sum += fibinary1[i] - '0';
-        }
-        if (i < fibinary2.size()) {
-            sum += fibinary2[i] - '0';
-        }
-        result.push_back(sum % 2 + '0');
-        carry = sum / 2;
+#define ll long long
+
+vector<int> fibonacci() {
+    vector<int> fib = {1, 2};
+    while (fib.back() <= 1e9) {
+        fib.push_back(fib[fib.size() - 1] + fib[fib.size() - 2]);
     }
-    if (carry) {
-        result.push_back(carry + '0');
+    return fib;
+}
+
+ll fibinary_to_decimal(string& fibinary, vector<int>& fib) {
+    ll decimal = 0;
+    for (int i = 0; i < fibinary.size(); i++) {
+        if (fibinary[fibinary.size() - 1 - i] == '1') {
+            decimal += fib[i];
+        }
     }
-    return result;
+    return decimal;
+}
+
+string decimal_to_fibinary(ll num, vector<int>& fib) {
+    string fibinary;
+    bool started = false;
+    for (int i = fib.size() - 1; i >= 0; i--) {
+        if (fib[i] <= num) {
+            num -= fib[i];
+            fibinary += '1';
+            started = true;
+        }
+        else if (started) {
+            fibinary += '0';
+        }
+    }
+    return fibinary.empty() ? "0" : fibinary;
 }
 
 signed main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
+    vector<int> fib = fibonacci();
     string fibinary1, fibinary2;
-    while (getline(cin, fibinary1) && getline(cin, fibinary2)) {
-        string sum = sum_fibinary(fibinary1, fibinary2);
-        cout << sum << '\n';
-        string blank;
-        if (getline(cin, blank) && blank.empty()) {
+    bool first = true;
+    while (cin >> fibinary1 >> fibinary2) {
+        if (!first) {
             cout << '\n';
         }
+        first = false;
+        ll decimal1 = fibinary_to_decimal(fibinary1, fib);
+        ll decimal2 = fibinary_to_decimal(fibinary2, fib);
+        cout << decimal_to_fibinary(decimal1 + decimal2, fib) << '\n';
     }
     return 0;
 }
